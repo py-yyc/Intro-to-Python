bd87ba916850e305ae8936d7fa6927808d6daa79
richatwork <34986359+richardworking@users.noreply.github.com>
2019-05-07 14:49:03 -0600

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,12 +1,12 @@
 
 #Running our first python program from an editor
 
-creat folder, py_yyc
-create file, hello_world.py
+#creat folder, py_yyc
+#create file, hello_world.py
 
 print('hello world')
 
-in command line:
+#in command line:
 
 cd py_yyc
 python hello_world.py
