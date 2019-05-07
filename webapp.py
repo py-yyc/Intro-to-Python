b81a19174357f11f3fd6f7a6b3505f8ea3ea9197
richatwork <34986359+richardworking@users.noreply.github.com>
2019-05-07 14:50:37 -0600

    Create webapp.py

---
+++ webapp.py
@@ -0,0 +1,13 @@
+# webapp.py, using Flask to deploy a browser on your localhost
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+app.route("/hello")
+def hello():
+    return "Hello World"
+
+if __name__=="__main__":
+    app.run(debug=True)
